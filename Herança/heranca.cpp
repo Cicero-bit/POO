e5ae13bdae325d17aaec4903f48b1f0e397d9f34
Cicero-bit <cicerosduelis@gmail.com>
2022-02-14 18:03:19 -0300

    fixes

--- Herança/heranca.cpp
+++ Herança/heranca.cpp
@@ -8,7 +8,7 @@ class Personagem{
     int vida = 100, ataque = 10, speeldmg = 10, mana = 10, stamina = 10;
 public:
     Personagem(){
-        std::cout << "BOOMM MAGIA";
+        std::cout << "Personagem ";
     }
 
     std::string getNome(){
@@ -31,7 +31,7 @@ public:
         std::cout << "Personagem morreu ;(" <<  std::endl;
     }
     void ReceberDano(int dano){
-        if(alive = true && vida > dano){
+        if(status() == true && vida > dano){
             vida -= dano;
         }else{
             std::cout << "Dano d+ ";
@@ -42,10 +42,19 @@ public:
 };
 class Warrior: public Personagem{
     std::string nome;
-    int vida = 100, ataque = 10, speeldmg = 5, mana = 10, stamina = 150;
+    int vida = 100, ataque = 20, speeldmg = 5, mana = 10, stamina = 150;
 public:
     Warrior(std::string nome = "") : nome{nome}{
         std::cout << nome << " -> Class Warrior / VIDA: " << vida << std::endl;
+    }
+    void ReceberDano(int dano){
+        if(status() == true && vida > dano){
+            vida -= dano;
+        }else{
+            std::cout << "Dano d+ ";
+            morrer();
+            vida = 0;
+        }
     }
     void atacar(Personagem* inimigo){
         if (stamina >= ataque){
@@ -63,10 +72,19 @@ public:
 
 class Mage: public Personagem{
     std::string nome;
-    int vida = 100, ataque = 5, speeldmg = 30, mana = 100, stamina = 50;
+    int vida = 100, ataque = 10, speeldmg = 30, mana = 100, stamina = 50;
 public:
     Mage(std::string nome = ""): nome{nome}{
         std::cout << nome << " -> Class Mage / VIDA: " << vida << std::endl;
+    }
+    void ReceberDano(int dano){
+        if(status() == true && vida > dano){
+            vida -= dano;
+        }else{
+            std::cout << "Dano d+ ";
+            morrer();
+            vida = 0;
+        }
     }
     void lancarSpell(Personagem* inimigo){
         if(mana >= speeldmg){
@@ -82,6 +100,8 @@ public:
     }
 };
 
+class Fire_Mage : public Mage{};
+
 int main(){
   
     Mage Jao{"Jao"};
@@ -90,8 +110,8 @@ int main(){
     Ze.atacar(&Jao);
     Jao.lancarSpell(&Ze);
 
-    std::cout << Jao.getVida() << std::endl;
-    std::cout << Ze.getVida() << std::endl;
+    std::cout << "Vida Jao depois do ataque: " << Jao.getVida() << std::endl;
+    std::cout << "Vida Ze depois do ataque: " << Ze.getVida() << std::endl;
 
     return 0;
 } 
